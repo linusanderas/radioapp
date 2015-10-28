49c2edc2b0df7b4f23e34cc95d2f8d86f50b3c1c
Linus <linus@improove.se>
2015-10-28 10:13:18 +0100

    Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,18 @@
+(function(){
+  'use strict';
+  var module = angular.module('app', ['onsen']);
+
+  module.controller('AppController', function($scope, $data) {
+   
+   
+  });
+
+
+  module.controller('MasterController', function($scope, $data) {
+   
+
+
+  });
+
+})();
+
